dd775f48d536b5e8e559523d4d04cbcb2155c0af
Rasha Zayed <zayed.rasha@outlook.com>
2020-11-23 17:49:18 -0800

    login form using auth0

---
+++ client/src/components/LoginButton.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {useAuth0} from '@auth0/auth0-react'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
+
+
+export default()=>{
+    const classes = useStyles();
+    const { loginWithRedirect ,isAuthenticated } = useAuth0();
+    
+    return(
+        !isAuthenticated &&(
+            
+            
+        <Button className={classes.root} onClick={()=> loginWithRedirect()}>
+            Login
+        </Button>
+        )
+
+    )
+}
